9cd4cbc563093cb8742416a883a90f770b3d6284
Zuhra-Masoud <136282916+Zuhra-Masoud@users.noreply.github.com>
2023-06-16 11:01:14 -0400

    Add files via upload

---
+++ array.js
@@ -0,0 +1,36 @@
+
+
+
+
+let groceries = ['bread', 'milk', 'eggs', 'ice cream', 'vegetables', 'cheese'];
+console.log(groceries[0]);
+console.log(groceries[5]);
+
+
+groceries.push("fruits");//Adding
+console.log(groceries);
+
+groceries.pop();//remove
+console.log(groceries);
+
+let getArrayLength = groceries.length;//length
+console.log(getArrayLength);
+
+
+let classRoster = [
+    ["Cheetah", 12, 88.7],
+    ["Che", 14, 55.6],
+    ["Dhe", 16, 85.3]
+];
+console.log(classRoster);//Whole array
+
+console.log(classRoster[0]);//each element of the array
+console.log(classRoster[1]);
+console.log(classRoster[2]);//0, 1, 2
+
+
+console.log(classRoster[0][0]);//each element of the array of array
+
+
+
+
